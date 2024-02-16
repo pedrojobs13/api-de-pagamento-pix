42b714127d49825909ba6522d7b1ab0dbf71a0f3
pedrojobs13 <pedro.lg.oliveira@edu.ufes.br>
2024-02-15 22:32:54 -0300

    cors

--- src/main/java/com/pedro/pagamento/api/controller/CriaPagamentoController.java
+++ src/main/java/com/pedro/pagamento/api/controller/CriaPagamentoController.java
@@ -57,7 +57,7 @@ public class CriaPagamentoController {
     verificaEAtualizaPagamentoService.recebeWebHookPagamento(jsonresponseBody);
   }
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin
   @ResponseStatus(HttpStatus.OK)
   @GetMapping("/pagadores")
   public List<ClienteModelDTO> buscaPagadoresConfirmados() {
